7433d959a5d45930d521bebdda1709f08cd7e1a4
lorenzo.pomili <pomili.lorenzo85@gmail.com>
2018-02-04 00:54:42 +0100

    added loader for bucklescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-    entry: './src/Main.js',
-    output: {
-      path: __dirname + '/public',
-      filename: 'bundle.js'
-    }
+  entry: './src/Main.re',
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      // Set up Reason and OCaml files to use the loader
+      { test: /\.(re|ml)$/, use: 'bs-loader' },
+    ]
+  },
+  resolve: {
+    // Add .re and .ml to the list of extensions webpack recognizes
+    extensions: ['.re', '.ml', '.js']
+  }
 };
