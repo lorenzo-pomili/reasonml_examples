4d9f492930ffb9e550de2a658d600d37e4aab0f1
lorenzo.pomili <pomili.lorenzo85@gmail.com>
2018-02-14 11:58:37 +0100

    fixed regex for test

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   verbode: true,
-  testMatch: [ '**/__tests__/**/*.js?(x)', '**/?(*_)(spec|test).js?(x)' ]
+  testMatch: [ '**/__tests__/**/*.js?(x)', '**/?(*_)(spec|test).bs.js?(x)' ]
 }
