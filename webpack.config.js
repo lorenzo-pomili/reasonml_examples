170bef547c2f7aa20d2f08c1065e1df0ac445f18
lorenzo.pomili <pomili.lorenzo85@gmail.com>
2018-03-23 12:53:06 +0100

    webpack optimization

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,14 @@ module.exports = {
   module: {
     rules: [
       // Set up Reason and OCaml files to use the loader
-      { test: /\.(re|rei|ml|mli)$/, use: 'bs-loader' },
+      {
+        test: /\.(re|rei|ml|mli)$/,
+        exclude: __dirname+'/node_modules/',
+        use: 'bs-loader'
+      },
       {
         test: /\.js$/,
+        exclude: __dirname+'/node_modules/',
         use: [{
           loader: 'babel-loader',
           options: {
